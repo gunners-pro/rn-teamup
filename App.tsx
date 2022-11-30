371e3c350ea6cae375b6fd11f00f5aada647afd8
Fabricyo Barreto <bass.barreto@gmail.com>
2022-11-30 20:16:52 -0300

    style: add NewGroup screen

--- App.tsx
+++ App.tsx
@@ -9,6 +9,7 @@ import { ThemeProvider } from 'styled-components/native';
 import { Loading } from '@components/Loading';
 
 import { Groups } from '@screens/Groups';
+import { NewGroup } from '@screens/NewGroup';
 
 import { theme } from '@theme/index';
 
@@ -22,7 +23,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <Groups /> : <Loading />}
+      {fontsLoaded ? <NewGroup /> : <Loading />}
     </ThemeProvider>
   );
 }
