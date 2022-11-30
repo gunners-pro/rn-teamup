e329e2f3d60e9a583e3ba616afa1c4b9c2ac46c1
Fabricyo Barreto <bass.barreto@gmail.com>
2022-11-30 19:52:22 -0300

    style: add GroupCard component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,13 +1,24 @@
+import { useState } from 'react';
+import { FlatList } from 'react-native';
+
+import { GroupCard } from '@components/GroupCard';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 
 import { Container } from './styles';
 
 export function Groups() {
+  const [groups, setGroups] = useState(['Galera do React-Native']);
+
   return (
     <Container>
       <Header />
       <Highlight title="Turmas" subtitle="jogue com a sua turma" />
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+      />
     </Container>
   );
 }
