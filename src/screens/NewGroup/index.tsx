4ed4439bc60cef4219da83e8533571ca56d4d55b
Fabricyo Barreto <bass.barreto@gmail.com>
2022-12-01 20:39:24 -0300

    feat: add exception handling to newGroup screen

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,5 +1,6 @@
 import { useNavigation } from '@react-navigation/native';
 import { useState } from 'react';
+import { Alert } from 'react-native';
 
 import { Button } from '@components/Button';
 import { Header } from '@components/Header';
@@ -7,6 +8,8 @@ import { Highlight } from '@components/Highlight';
 import { Input } from '@components/Input';
 
 import { groupCreate } from '@storage/group/groupCreate';
+
+import { AppError } from '@utils/AppError';
 
 import { Container, Content, Icon } from './styles';
 
@@ -16,10 +19,18 @@ export function NewGroup() {
 
   async function handleNew() {
     try {
+      if (group.trim().length === 0) {
+        return Alert.alert('Novo Grupo', 'Informe o nome da turma.');
+      }
+
       await groupCreate(group);
       navigate('players', { group });
     } catch (error) {
-      console.log(error);
+      if (error instanceof AppError) {
+        Alert.alert('Novo Grupo', error.message);
+      } else {
+        Alert.alert('Novo Grupo', 'Não foi possivel criar um novo grupo.');
+      }
     }
   }
 
--- src/storage/group/groupCreate.ts
+++ src/storage/group/groupCreate.ts
@@ -2,11 +2,20 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import { GROUP_COLLECTION } from '@storage/storageConfig';
 
+import { AppError } from '@utils/AppError';
+
 import { groupGetAll } from './groupGetAll';
 
 export async function groupCreate(groupName: string) {
   try {
     const storedGroups = await groupGetAll();
+
+    const groupAlreadyExists = storedGroups.includes(groupName);
+
+    if (groupAlreadyExists) {
+      throw new AppError('Já existe um grupo cadastrado com esse nome.');
+    }
+
     const storage = JSON.stringify([...storedGroups, groupName]);
     await AsyncStorage.setItem(GROUP_COLLECTION, storage);
   } catch (error) {
