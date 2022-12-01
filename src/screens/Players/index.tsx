6989c1487d1718a4c9e053afcd2aa38e8c65aea7
Fabricyo Barreto <bass.barreto@gmail.com>
2022-11-30 21:38:28 -0300

    style: add ButtonIcon component

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,7 +1,9 @@
+import { ButtonIcon } from '@components/ButtonIcon';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
+import { Input } from '@components/Input';
 
-import { Container } from './styles';
+import { Container, Form } from './styles';
 
 export function Players() {
   return (
@@ -11,6 +13,10 @@ export function Players() {
         title="Nome da turma"
         subtitle="adicione a galera e separe os times"
       />
+      <Form>
+        <Input placeholder="Nome da pessoa" />
+        <ButtonIcon icon="add" />
+      </Form>
     </Container>
   );
 }
