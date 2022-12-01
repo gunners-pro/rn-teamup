46ab47a967037fa08771a5634f43ceb8edaf0c3a
Fabricyo Barreto <bass.barreto@gmail.com>
2022-11-30 21:56:36 -0300

    style: add Filter component

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,11 +1,18 @@
+import { useState } from 'react';
+import { FlatList } from 'react-native';
+
 import { ButtonIcon } from '@components/ButtonIcon';
+import { Filter } from '@components/Filter';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { Input } from '@components/Input';
 
-import { Container, Form } from './styles';
+import { Container, Form, HeaderList, NumberOfPlayers } from './styles';
 
 export function Players() {
+  const [team, setTeam] = useState('time a');
+  const [players, setPlayers] = useState([]);
+
   return (
     <Container>
       <Header showBackButton />
@@ -17,6 +24,22 @@ export function Players() {
         <Input placeholder="Nome da pessoa" />
         <ButtonIcon icon="add" />
       </Form>
+      <HeaderList>
+        <FlatList
+          data={['time a', 'time b', 'time c', 'time d', 'time e', 'time f']}
+          keyExtractor={item => item}
+          renderItem={({ item }) => (
+            <Filter
+              title={item}
+              isActive={item === team}
+              onPress={() => setTeam(item)}
+            />
+          )}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+        />
+        <NumberOfPlayers>{players.length}</NumberOfPlayers>
+      </HeaderList>
     </Container>
   );
 }
